21da0697b2ce55c2f82dc04ccfb6b07943eacaee
Glaucia Lemos <glaucia_lemos86@hotmail.com>
2018-07-18 00:15:10 -0300

    Alterações significativas.

--- src/index.js
+++ src/index.js
@@ -6,3 +6,19 @@
  * Author: Glaucia Lemos
  *
  */
+
+var d3 = require("d3");
+
+var data = [30, 86, 168, 281, 303, 365];
+
+d3.select(".chart")
+    .selectAll("div")
+    .data(data)
+    .enter()
+    .append("div")
+    .style("width", function (d) {
+        return d + 'px'
+    })
+    .text(function (d) {
+        return '$ ' + d;
+    });
